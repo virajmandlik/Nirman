65113e9fb6a55609b5d423b5fd213c77478a4692
Viraj Mandlik <138781016+virajmandlik@users.noreply.github.com>
2025-04-11 20:50:36 +0530

    Delete v04 directory

--- v04/backend/src/middleware/asyncHandler.js
+++
@@ -1,9 +0,0 @@
-/**
- * Async handler middleware to avoid try/catch blocks in route handlers
- * @param {Function} fn Function that needs to be wrapped with try/catch
- * @returns {Function} Express middleware
- */
-const asyncHandler = fn => (req, res, next) =>
-  Promise.resolve(fn(req, res, next)).catch(next);
-
-module.exports = asyncHandler; 
